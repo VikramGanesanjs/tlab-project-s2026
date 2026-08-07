ddabbbd9fb7fed2425bc9531728403385f62f8b8
Vikram Ganesan <96213191+VikramGanesanjs@users.noreply.github.com>
2026-08-06 17:10:59 -0700

    trying to debug inference

--- src/run_dino_mst_3.sh
+++ src/run_dino_mst_3.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=dinov3_mst
 #SBATCH -p gpu
-#SBATCH --gpus=1
-#SBATCH --cpus-per-gpu=8
+#SBATCH --gpus=l40s:2
+#SBATCH --cpus-per-gpu=2
 #SBATCH --mem=120G
 #SBATCH --time 48:00:00        
-#SBATCH --error=run_dino_mst_ssl_finetune_8slices.err  ## error log file
-#SBATCH --output=run_dino_mst_ssl_finetune_8slices.out ## output log file
+#SBATCH --error=run_dino_mst_ssl_finetune_128slices_noaug.err  ## error log file
+#SBATCH --output=run_dino_mst_ssl_finetune_128slices_noaug.out ## output log file
 #SBATCH --mail-user=Vikram.Ganesan@cshs.org
 #SBATCH --mail-type=ALL
 
@@ -17,4 +17,4 @@ conda activate dinov3
 
 cd /common/ganesanv/tlab/src
 
-python dino_mst.py --params-file /common/ganesanv/tlab/runs/mst_configs/adni/ssl_finetune_adni_8slices.yaml
+python dino_mst.py --params-file /common/ganesanv/tlab/runs/mst_configs/adni/ssl_finetune_adni_128slices_noaugment.yaml
